78bfe3e51214a01167f70d9ac34d399b81a28130
Francis <numofran6@gmail.com>
2023-01-13 09:39:04 +0000

    0/15

--- components/Kids.jsx
+++ components/Kids.jsx
@@ -19,7 +19,7 @@ export const Kids = () => {
 				<div className="max-w-[12rem] sm:max-w-xs lg:max-w-lg ml-5 sm:ml-10 lg:ml-28 h-full flex flex-col justify-center space-y-5 items-center">
 					<h1 className="font-bold uppercase text-xl md:text-4xl">kids</h1>
 
-					<p className="text-gray-100 text-xs sm:text-sm sm:leading-[1.2rem] text-center">
+					<p className="text-gray-100 text-xs sm:text-sm text-center">
 						We love kids! We have Pastors who are richly educated in the Word of
 						God and who teach kids from the age of 5 - 15 about the wonderful
 						word of God.
