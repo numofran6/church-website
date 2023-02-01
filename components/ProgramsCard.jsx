58fca870bac01ee9024cbe0951a8cf736b7bdb24
Francis <numofran6@gmail.com>
2023-02-01 08:49:18 +0000

    programscard modified

--- components/ProgramsCard.jsx
+++ components/ProgramsCard.jsx
@@ -7,7 +7,7 @@ export const ProgramsCard = ({ day, icon, time, serviceName }) => {
 			<Image
 				src={icon}
 				alt=""
-				className="w-16 h-16 sm:w-20 sm:h-20 bg-stone-300 p-2 sm:p-4 rounded-lg"
+				className="w-16 h-16 sm:w-20 sm:h-20 bg-stone-300 p-2 sm:p-4"
 			/>
 			<h1 className="text-gray-800 font-bold mb-2 text-sm">{serviceName}</h1>
 			<h1 className="text-gray-600">{day}</h1>
