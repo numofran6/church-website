ea260e6278e6e3cbf99de09cafcb7822f864c1dc
Francis <numofran6@gmail.com>
2023-02-06 16:55:32 +0000

    Header image changed

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ function Home() {
 				<title>Glorious Hill</title>
 			</Head>
 
-			<motion.section id="home" className="bg-yellow-700">
+			<motion.section id="home" className="bg-cyan-600">
 				<Header />
 			</motion.section>
 
